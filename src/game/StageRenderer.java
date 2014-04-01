719c8f043be3e0d3a2d4c1f5465c3179df94beb8
Yike Liu <okmilkd@gmail.com>
2014-03-31 23:24:54 -0400

    Update StageRenderer.java

--- src/game/StageRenderer.java
+++ src/game/StageRenderer.java
@@ -14,7 +14,25 @@ public class StageRenderer {
 	private BufferedImage bar1 = loader.loadImage("/image/stage/stage1/bar1.png");
 	private BufferedImage bar2 = loader.loadImage("/image/stage/stage1/bar2.png");
 	private BufferedImage hyoushiki = loader.loadImage("/image/stage/stage1/hyoushiki.png");
+	private BufferedImage amedama = loader.loadImage("/image/stage/stage2/amedama.png");
+	private BufferedImage ashiba1 = loader.loadImage("/image/stage/stage2/ashiba1.png");
+	private BufferedImage ashiba2 = loader.loadImage("/image/stage/stage2/ashiba2.png");
+	private BufferedImage ashiba3 = loader.loadImage("/image/stage/stage2/ashiba3.png");
+	private BufferedImage ashiba4 = loader.loadImage("/image/stage/stage2/ashiba4.png");
+	private BufferedImage candy = loader.loadImage("/image/stage/stage2/candy.png");
+	private BufferedImage cyusya1 = loader.loadImage("/image/stage/stage2/cyusya1.png");
+	private BufferedImage cyusya2 = loader.loadImage("/image/stage/stage2/cyusya2.png");
+	private BufferedImage no3 = loader.loadImage("/image/stage/stage2/no3.png");
+	private BufferedImage no2 = loader.loadImage("/image/stage/stage2/no2.png");
+	private BufferedImage no6 = loader.loadImage("/image/stage/stage2/no6.png");
+	private BufferedImage no4 = loader.loadImage("/image/stage/stage2/no4.png");
+	private BufferedImage syujuturamp = loader.loadImage("/image/stage/stage2/syujuturamp.png");
+	private BufferedImage tv1 = loader.loadImage("/image/stage/stage3/tv1.png");
+	private BufferedImage tv2 = loader.loadImage("/image/stage/stage3/tv2.png");
+	private BufferedImage tv3 = loader.loadImage("/image/stage/stage3/tv3.png");
 
+	
+	
 	public void render1(Graphics g){
 		g.drawImage(corn, 14*size, 0*size,size,size, null);
 		g.drawImage(corn, 10*size, 2*size,size,size, null);	
@@ -41,22 +59,79 @@ public class StageRenderer {
 		g.drawImage(bara, 7*size, 5*size,size,size, null);
 		g.drawImage(bara, 8*size, 5*size,size,size, null);
 	}
+	
 	public void render3(Graphics g){
-		
+		g.drawImage(corn, 0*size, 0*size,size,size, null);
+		g.drawImage(corn, 15*size, 0*size,size,size, null);
+		g.drawImage(corn, 0*size, 9*size,size,size, null);
+		g.drawImage(corn, 15*size, 9*size,size,size, null);
+		g.drawImage(bar1, 4*size, 7*size,3*size,size, null);
+		g.drawImage(bar1, 9*size, 7*size,3*size,size, null);
+		g.drawImage(hyoushiki, 7*size, 7*size,size,size, null);
+		g.drawImage(hyoushiki, 8*size, 7*size,size,size, null);
+		g.drawImage(bara, 2*size, 3*size,size,size, null);
+		g.drawImage(bara, 13*size, 3*size,size,size, null);
+		g.drawImage(bara, 2*size, 6*size,size,size, null);
+		g.drawImage(bara, 13*size, 6*size,size,size, null);
 	}
 	public void render4(Graphics g){
-		
+		g.drawImage(candy, 0*size, 0*size,size,2*size, null);
+		g.drawImage(no2, 2*size, 3*size,size,size, null);
+		g.drawImage(cyusya1, 13*size, 0*size,size,2*size, null);
+		g.drawImage(no4, 2*size, 6*size,size,size, null);
+		g.drawImage(no6, 13*size, 3*size,size,size, null);
+		g.drawImage(no3, 13*size, 6*size,size,size, null);
+		g.drawImage(amedama, 0*size, 9*size,size,size, null);
+		g.drawImage(amedama, 15*size, 9*size,size,size, null);
+		g.drawImage(ashiba1, 4*size, 7*size,2*size,size, null);
+		g.drawImage(ashiba4, 10*size, 7*size,2*size,size, null);
 	}
 	public void render5(Graphics g){
-		
+		g.drawImage(candy, 0*size, 0*size,size,2*size, null);
+		g.drawImage(syujuturamp, 7*size, 0*size,2*size,size, null);
+		g.drawImage(syujuturamp, 7*size, 9*size,2*size,size, null);
+		g.drawImage(ashiba2, 7*size, 3*size,2*size,size, null);
+		g.drawImage(ashiba3, 7*size, 6*size,2*size,size, null);
+		g.drawImage(cyusya1, 0*size, 8*size,size,2*size, null);
 	}
 	public void render6(Graphics g){
-		
+		g.drawImage(no3, 3*size, 2*size,size,size, null);
+		g.drawImage(no6, 12*size, 2*size,size,size, null);
+		g.drawImage(cyusya1, 6*size, 4*size,size,2*size, null);
+		g.drawImage(cyusya2, 9*size, 4*size,size,2*size, null);
+		g.drawImage(amedama, 12*size, 7*size,size,size, null);
 	}
 	public void render7(Graphics g){
-			
-		}
+		g.drawImage(tv2, 0*size, 1*size,size,size, null);
+		g.drawImage(tv2, 1*size, 1*size,size,size, null);
+		g.drawImage(tv1, 7*size, 2*size,size,size, null);
+		g.drawImage(tv1, 15*size, 4*size,size,size, null);
+		g.drawImage(tv1, 8*size, 2*size,size,size, null);
+		g.drawImage(tv3, 1*size, 4*size,4*size,size, null);
+		g.drawImage(tv3, 2*size, 6*size,4*size,size, null);
+		g.drawImage(tv3, 7*size, 8*size,4*size,size, null);
+	}
+	
 	public void render8(Graphics g){
-		
+		g.drawImage(tv1, 3*size, 2*size,size,size, null);
+		g.drawImage(tv1, 4*size, 2*size,size,size, null);
+		g.drawImage(tv1, 5*size, 2*size,size,size, null);
+		g.drawImage(tv3, 8*size, 4*size,4*size,size, null);
+		g.drawImage(tv3, 5*size, 8*size,4*size,size, null);
+		g.drawImage(tv1, 2*size, 4*size,size,size, null);
+		g.drawImage(tv1, 2*size, 5*size,size,size, null);
+		g.drawImage(tv2, 11*size, 0*size,size,size, null);
+		g.drawImage(tv2, 12*size, 0*size,size,size, null);
+		g.drawImage(tv2, 13*size, 0*size,size,size, null);
+		g.drawImage(tv1, 2*size, 6*size,size,size, null);
+		g.drawImage(tv1, 0*size, 8*size,size,size, null);
+		g.drawImage(tv1, 0*size, 9*size,size,size, null);
+		g.drawImage(tv1, 15*size, 7*size,size,size, null);
+		g.drawImage(tv1, 15*size, 6*size,size,size, null);
+		g.drawImage(tv2, 2*size, 9*size,size,size, null);
+	}
+	
+    public void render9(Graphics g){
+    	
 	}
 }
