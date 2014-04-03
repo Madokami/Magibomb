f952b44b730db297648ab6e8d28b382314e03be1
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 01:05:31 -0400

    Update SaDash.java

--- src/gameObject/SaDash.java
+++ src/gameObject/SaDash.java
@@ -5,6 +5,15 @@ import gameObject.GameObject.ORIENTATION;
 
 import java.awt.Graphics;
 
+/**
+* <b>Description:</b>
+* <br>
+* Defines projectile movmement with changing velocities
+* <br>Requires input of coordinates, and outputs direction and speed of projectile
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class SaDash extends Projectile{
 	int duration;
 	int durationTimer;
