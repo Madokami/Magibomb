b74887de4fca64351e1a7b03c906160f10eb384e
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 01:08:16 -0400

    Update TiroFinale.java

--- src/gameObject/TiroFinale.java
+++ src/gameObject/TiroFinale.java
@@ -7,6 +7,15 @@ import java.awt.Graphics;
 
 import system.GameSystem;
 
+/**
+* <b>Description:</b>
+* <br>
+* Defines individual projectile
+* <br>Requires input of coordinates, and outputs direction and speed of projectile
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class TiroFinale extends Projectile{
 	public TiroFinale(int x, int y, Game game, GameObject o) {
 		super(x, y, game, o);
