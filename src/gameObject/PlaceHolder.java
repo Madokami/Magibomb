1ad78cad11193483605ca6e5d3e81e560f45955a
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 00:46:45 -0400

    Update PlaceHolder.java

--- src/gameObject/PlaceHolder.java
+++ src/gameObject/PlaceHolder.java
@@ -2,6 +2,14 @@ package gameObject;
 
 import game.Game;
 
+/**
+* <b>Description:</b>
+* <br>
+* PlaceHolder class that extends Brick
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class PlaceHolder extends Brick{
 
 	public PlaceHolder(int x, int y, Game game) {
