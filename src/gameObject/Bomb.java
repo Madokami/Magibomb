9ee60e44944aae5e791c92446114ef7869e95d89
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-02 21:33:28 -0400

    Update Bomb.java

--- src/gameObject/Bomb.java
+++ src/gameObject/Bomb.java
@@ -4,6 +4,17 @@ import game.Game;
 
 import java.util.LinkedList;
 
+/**
+* <b>Description:</b>
+* <br><br>
+* Dislayment of bomb at appropriate time and location on map grid
+* <br>Bomb is designed to appear when it is dropped by character at user's command
+* <br>Bomb is placed at exact coordinates of the character
+* <br>Bomb should disappear when detonated
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class Bomb extends MovableObject{
 	public int counter;
 	public int explodeTime;
@@ -13,7 +24,17 @@ public class Bomb extends MovableObject{
 	public String direction;
 	public LinkedList<MovableObject> initiallyOnBomb=new LinkedList<MovableObject>();
 	
-	
+	/**
+	 * Responsible for acquiring bomb image from sprite sheet and detonating at specified time
+	 * <br>Includes effects such as damage to enemies and player character
+	 * <br><br>
+	 * <b>Inputs:</b>
+	 * <br><b>x</b>, <b>y</b> - coordinates for where bomb should be placed
+	 * <br><b>game</b> - Game object
+	 * <br><b>bombStrength</b> - the amount of health the character loses from the bomb
+	 * <br><b>bombLength</b> - the size of the bomb
+	 * <br><b>duration</b> - the time that the bomb is present before detonation
+	 */
 	public Bomb(int x,int y, Game game,int bombStrength,int bombLength,int duration){
 		super(x,y,game);
 		
@@ -44,6 +65,10 @@ public class Bomb extends MovableObject{
 			initiallyOnBomb.add(enemies.get(i));
 		}
 	}
+	
+	/**
+	 * Incremental method that determines the next process based on the current status quo
+	 */
 	public void tick(){
 		super.tick();
 		for(int i=0;i<initiallyOnBomb.size();i++){
@@ -59,11 +84,19 @@ public class Bomb extends MovableObject{
 			}
 		}
 	}
+	
+	/**
+	 * The bomb's explosion effects grids within specific lengths from point of explosion
+	 */
 	public void explode(){
 		game.getController().createExplosion((int)super.xGridNearest, (int)super.yGridNearest, length, strength);
 		game.getController().removeEntity(this);
 		game.getPlayer().bombCount++;
 	}
+	
+	/**
+	 * The bomb disappears after it is detonated
+	 */
 	public void removeFromMap(){
 		boolean[][] map = game.getBombArray();
 		map[xStarting][yStarting]=false;
