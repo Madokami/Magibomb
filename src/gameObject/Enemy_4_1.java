aef505739f4e07a452b071ef495450ee09cb207f
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-02 22:54:32 -0400

    Update Enemy_4_1.java

--- src/gameObject/Enemy_4_1.java
+++ src/gameObject/Enemy_4_1.java
@@ -2,6 +2,16 @@ package gameObject;
 
 import game.Game;
 
+/**
+* <b>Description:</b>
+* <br>
+* New type of enemy is defined with corresponding attributes such as speed and abilites
+* <br>Enemy spawns at specific coordinates on grid map
+* <br>Enemy is unique and displayed with images corresponding to its animation
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class Enemy_4_1 extends Enemy_4{
 
 	public Enemy_4_1(int x, int y, Game game) {
