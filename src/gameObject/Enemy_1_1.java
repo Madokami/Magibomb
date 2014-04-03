8c3056edf17ad420acfe2dfa205df2b4c96c0d5e
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-02 22:49:53 -0400

    Update Enemy_1_1.java

--- src/gameObject/Enemy_1_1.java
+++ src/gameObject/Enemy_1_1.java
@@ -9,6 +9,7 @@ import java.awt.Graphics;
 * <br>
 * New type of enemy is defined with corresponding attributes such as speed and abilites
 * <br>Enemy spawns at specific coordinates on grid map
+* <br>Enemy is unique and displayed with images corresponding to its animation
 * @author Team 6
 * @version 1.0
 * @since 2014-03-31
