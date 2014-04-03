e6a428ed1248afdc7177baa7a6ce52c33c77b6bf
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 00:51:59 -0400

    Update PlayerVoice.java

--- src/gameObject/PlayerVoice.java
+++ src/gameObject/PlayerVoice.java
@@ -1,4 +1,13 @@
 package gameObject;
+
+/**
+* <b>Description:</b>
+* <br>
+* PlayerVoice interface
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public interface PlayerVoice {
 	public void playDeathSound();
 	public void playLevelUpSound();
