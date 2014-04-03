2ea071ec7f2a27238b09619b9638fc4fb71a6ca6
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 01:16:17 -0400

    Update MultiplayerStats.java

--- src/game/MultiplayerStats.java
+++ src/game/MultiplayerStats.java
@@ -1,5 +1,13 @@
 package game;
 
+/**
+* <b>Description:</b>
+* <br>
+* Defines multiplayer statistics
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class MultiplayerStats {
 	public static double HP,MP,SOUL,EXP;
 	public static int BOMBS,BOMBL,SPD,LEVEL,BP;
