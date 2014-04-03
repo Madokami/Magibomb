239199424728b86cc9125bf253d95fbadb303052
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 00:24:58 -0400

    Update HoVoice.java

--- src/gameObject/HoVoice.java
+++ src/gameObject/HoVoice.java
@@ -4,6 +4,15 @@ import java.util.Random;
 
 import system.GameSystem;
 
+/**
+* <b>Description:</b>
+* <br>
+* Responsible for playing different sounds
+* <br>ex. LevelUpSound, DeathSound
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class HoVoice implements PlayerVoice{
 	Random rand;
 	public HoVoice(){
