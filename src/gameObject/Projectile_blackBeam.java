7fdf0454a9ecd50928ab913e4eeecb14fc7e09f5
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-03 01:04:16 -0400

    Update Projectile_blackBeam.java

--- src/gameObject/Projectile_blackBeam.java
+++ src/gameObject/Projectile_blackBeam.java
@@ -3,6 +3,15 @@ package gameObject;
 import system.GameSystem;
 import game.Game;
 
+/**
+* <b>Description:</b>
+* <br>
+* Defines individual projectile
+* <br>Requires input of coordinates, and outputs direction and speed of projectile
+* @author Team 6
+* @version 1.0
+* @since 2014-03-31
+*/
 public class Projectile_blackBeam extends Projectile{
 
 	public int counter;
