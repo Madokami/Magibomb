cfa19865f42f08f9a539bc84743bbea4e0adfe32
tantany <karenyang08@hotmail.com>
2014-04-06 19:20:37 -0400

    Update GameData.java

--- src/system/GameData.java
+++ src/system/GameData.java
@@ -1,5 +1,11 @@
 package system;
-
+/**
+* Description:
+* Update game level and player data
+* @author Team 6
+* @version 1.0
+* @since 2014-03-27
+*/
 import game.Game;
 import game.PlayerData;
 
@@ -25,12 +31,20 @@ public class GameData implements Serializable{
 	private LinkedList<WallInterface> w = new LinkedList<WallInterface>();
 	*/
 	
+	/**
+	 * Update current level and player data
+	 * @param game current game
+	 */
 	public void updateGameData(Game game){
 		this.curLevel = game.getCurLevel();
 		this.pData=game.getPlayerData();
 
 	}
-
+	
+	/**
+	 * Set current level and player data
+	 * @param game current game
+	 */
 	public void loadGame(Game game) {
 		game.setCurLevel(curLevel);
 		game.setPlayerData(this.pData);
