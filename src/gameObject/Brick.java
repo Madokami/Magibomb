adaebae93541dc7ad3f4099f56a87740a67825c7
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-07 00:18:28 -0400

    Update Brick.java

--- src/gameObject/Brick.java
+++ src/gameObject/Brick.java
@@ -20,6 +20,7 @@ public abstract class Brick extends GameObject{
 	 * <b>Inputs:</b>
 	 * <b>x</b>,<b>y</b> - coordinates of brick image
 	 * <b>game</b> - Game object
+	 * @panam coordinates, game object
 	 */
 	public Brick(int x, int y, Game game) {
 		super(x, y, game);
