aa737deb9cca6c1ce94d8d0201e49f10f729daef
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-07 01:51:43 -0400

    Update Enemy_Boss_1.java

--- src/gameObject/Enemy_Boss_1.java
+++ src/gameObject/Enemy_Boss_1.java
@@ -19,6 +19,7 @@ public class Enemy_Boss_1 extends Enemy{
 	/**
 	 * defines new enemy boss
 	 * @panam coordinates, game object
+	 * @return enemy
 	 */
 	public Enemy_Boss_1(int x, int y, Game game) {
 		super(x, y, game);
