776e77ebe493708688dd4b10ddcc6939ec305761
Eddy Lu <eddy.lu@mail.mcgill.ca>
2014-04-06 23:09:15 -0400

    Update PowerUps.java

--- src/gameObject/PowerUps.java
+++ src/gameObject/PowerUps.java
@@ -12,16 +12,24 @@ import game.Game;
 */
 public abstract class PowerUps extends GameObject{
 	public String type;
+	/**
+	 * defines power up
+	 * @param coordinates, game object
+	 */
 	public PowerUps(int x, int y, Game game) {
 		super(x, y, game);
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * applies quick return
+	 */
 	public void tick(){
 		return;
 	}
 	
 	public abstract void applyEffect(Player player);
 	
+	//removes entity
 	public void remove(){
 		game.getController().removeEntity(this);
 	}
